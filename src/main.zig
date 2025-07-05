ed665acadc0aee7a2634d08cb5deda921ac17a22
Egor Rudkov <gren236@gmail.com>
2025-07-05 19:47:36 +0200

    add 200 response

--- src/main.zig
+++ src/main.zig
@@ -2,14 +2,14 @@ const std = @import("std");
 const net = std.net;
 
 pub fn main() !void {
-    const stdout = std.io.getStdOut().writer();
-
     const address = try net.Address.resolveIp("127.0.0.1", 4221);
     var listener = try address.listen(.{
         .reuse_address = true,
     });
     defer listener.deinit();
 
-    _ = try listener.accept();
-    try stdout.print("client connected!", .{});
+    const conn = try listener.accept();
+    defer conn.stream.close();
+
+    _ = try conn.stream.write("HTTP/1.1 200 OK\r\n\r\n");
 }
